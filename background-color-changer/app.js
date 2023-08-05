0c634d48a9e7af123ad468d53522d94804445bda
A114477s <nazmunsakib>
2023-08-05 07:32:48 -0700

    Change background color on click

--- background-color-changer/app.js
+++ background-color-changer/app.js
@@ -1,12 +1,38 @@
-const btnColorChange = document.querySelector('.change-color');
 
-function generateRandomNumber(n) {
-  return Math.floor(Math.random() * n);
+/*
+* Change background color with random rgb color
+*/
+
+window.onload = () => {
+  main();
 }
 
-btnColorChange.addEventListener('click', () => {
-  const r = generateRandomNumber(255);
-  const g = generateRandomNumber(255);
-  const b = generateRandomNumber(255);
-  document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-});
+/*
+* The main function
+*/
+function main(){
+  const siteMain = document.getElementById('site-main');
+  const changeBG = document.getElementsByClassName('change-color');
+
+  for( let btn of changeBG ){
+    btn.addEventListener('click',() => {
+      const bgColor = generateRGB();
+      siteMain.style.backgroundColor = bgColor;
+    });
+  }
+
+}
+
+/*
+* Random RGB genaretor
+*/
+const generateRGB = () => {
+  const r = Math.floor( Math.random() * 255 );
+  const g = Math.floor( Math.random() * 255 );
+  const b = Math.floor( Math.random() * 255 );
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+
+
